e7549f041b2055919a80273a547fac982f3d6934
rpirritano <rpirritano@gmail.com>
2016-12-29 07:40:33 -0800

    fixed problem of alblum list not appearing

--- scripts/album.js
+++ scripts/album.js
@@ -30,15 +30,15 @@
      ]
  };
 
- var createSongRow = function(songNumber, songName, songLength) {
+var createSongRow = function(songNumber, songName, songLength) {
      var template =
-        '<tr class="album-view-song-item">'
+         '<tr class="album-view-song-item">'
      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
      + '  <td class="song-item-title">' + songName + '</td>'
      + '  <td class="song-item-duration">' + songLength + '</td>'
      + '</tr>'
-      ;
- 
+     ;
+
      return template;
  };
 
@@ -68,35 +68,29 @@
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
 
+
 //Album button templates
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
- };
-
-songListContainer.addEventListener('mouseover', function(event) {
+     songListContainer.addEventListener('mouseover', function(event) {
     // Only target individual song rows during event delegation
-    if(event.target.parentElement.className === 'album-view-song-item') {
-        event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
-    }
-    ;
-});
+    if (event.target.parentElement.className === 'album-view-song-item') {
+    event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
 
-for (var i = ; i < songRows.length; i++) {
-    songRows[i].addEventListener('mouseleave', function(event) {
-        //Revert the content back to the number
+     for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             // Revert the content back to the number
         //Selects the first element, which is the song-item-number element
         this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
-    });
-    
-    }
-}
+         });
+     }
 
+ }
 
-var createSongRow = function(songNumber, songName, songLength) {
-    var teomplate
-}
 
 
 
